d4397be43d28ed804e5f95e4c18e98f94e92ab98
Jeffrey Fall <jfall@nvidia.com>
2021-07-14 20:26:31 -0700

    hardcoded webapp base directory of /home/jenkins/stageTomcat/target/bin/webapp

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -19,8 +19,10 @@ public class Main {
 
         tomcat.setPort(Integer.valueOf(webPort));
 
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        //tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        tomcat.addWebapp("/home/jenkins/stageTomcat/target/bin/webapp"); 
+        //System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: /home/jenkins/stageTomcat/target/bin/webapp");
 
         tomcat.start();
         tomcat.getServer().await();
